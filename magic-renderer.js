ec33489fde4407749f4dda7b89fac0e74ff8bc8f
Sebastián Caraballo <sebcarpr@yahoo.com.ar>
2023-10-05 16:03:52 -0300

    Add support for forms, buttons, links, loadings

--- magic-renderer.js
+++ magic-renderer.js
@@ -3,7 +3,10 @@ const magicRenderer = (prefix) => {
         for (const prop in data){
             const $targets = document.querySelectorAll(prop);
             if ($targets.length > 0) {
-                $targets.forEach($target => $target.innerHTML = data[prop]);
+                for (const $target of $targets) {
+                    $target.innerHTML = data[prop]; 
+                    console.log(data[prop]);
+                }
             }
         }
         magicRenderer(prefix);
@@ -20,6 +23,14 @@ const magicRenderer = (prefix) => {
         }
         async function formEvent(ev) {
             ev.preventDefault();
+            if (ev.target.isSending) {
+                return false;
+            }
+            ev.target.isSending = true;
+            let $loading = null;
+            if (ev.target.hasAttribute(`${prefix}-loading`)){
+                $loading = document.querySelector(ev.target.getAttribute(`${prefix}-loading`));
+            }
             const fetchOptions = {
                 method: ev.target.method,
             }
@@ -30,7 +41,11 @@ const magicRenderer = (prefix) => {
             } else {
                 url += '?' + new URLSearchParams(formData).toString();
             }
+            if ($loading) {
+                ev.target.innerHTML = $loading.innerHTML;
+            }
             const response = await fetch(url, fetchOptions);
+            ev.target.isSending = false;
             if (response.ok) {
                 const data = await response.json();
                 render(data);
